d762442d8df96a81c3676cecd32bdb757c664b34
ekildishev <ekildisev@gmail.com>
2019-12-13 14:20:04 +0300

    Attempt to fix checks

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*"`
-filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order
+files=`find . -name "*.cpp" -or -name "*.hpp" -or -name ".h" | grep -v "./tools/*" | grep -v "PicoSHA2"`
+filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order,-runtime/threadsafe_fn,-legal/copyright
 echo $files | xargs cpplint --filter=$filter
 
 export CTEST_OUTPUT_ON_FAILURE=true
